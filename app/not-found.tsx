3282fb7e93e84943d55801c97d8bc049b74edbde
judoree <135936771+judoree@users.noreply.github.com>
2024-05-13 16:17:59 +0900

    Not Found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,10 +1,10 @@
 import Navigation from "../components/navigation";
 
-export default function Page() {
+export default function NotFound() {
   return (
     <div>
       <Navigation />
-      <h1>Home!</h1>
+      <h1> Not Found page</h1>
     </div>
   );
 }
