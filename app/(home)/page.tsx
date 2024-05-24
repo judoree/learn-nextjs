859278f9cad3013849d8488497561acd13733eee
judoree <135936771+judoree@users.noreply.github.com>
2024-05-24 15:58:28 +0900

    Loading Component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,14 +3,12 @@ export const metadata = {
 };
 
 const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
-// server component fetch 할 함수
 async function getMovies() {
-  await new Promise((resolve) => setTimeout(resolve, 5000));
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   const response = await fetch(URL);
   const json = await response.json();
   return json;
 }
-// async 쓰는 이유 : await 쓰기 위함 기본적임 부모 함수에 async 써줘야함
 export default async function HomePage() {
   const movies = await getMovies();
   return <div>{JSON.stringify(movies)}</div>;
