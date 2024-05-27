1b7edea5ba194cbad8c47a6713ef3b9c1ca731cb
judoree <135936771+judoree@users.noreply.github.com>
2024-05-27 17:31:34 +0900

    CSS Modules

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,5 +1,5 @@
-import Link from "next/link";
-
+import Movie from "../../components/movie";
+import styles from "../../styles/home.module.css";
 export const metadata = {
   title: "Home",
 };
@@ -7,7 +7,7 @@ export const metadata = {
 export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 async function getMovies() {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
+  // await new Promise((resolve) => setTimeout(resolve, 1000));
   const response = await fetch(API_URL);
   const json = await response.json();
   return json;
@@ -17,9 +17,9 @@ export default async function HomePage() {
   return (
     <div>
       {movies.map((movie) => (
-        <li key={movie.id}>
-          <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
-        </li>
+        <div className={styles.container}>
+          <Movie key={movie.id} id={movie.id} poster_path={movie.poster_path} title={movie.title} />
+        </div>
       ))}
     </div>
   );
